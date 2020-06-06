13ee1f4c6e7d7e46c354d7fb3c34438bda453aa0
Matthias Bauw <matthias.bauw@gmail.com>
2020-06-06 17:16:12 +0200

    Updated to Drupal9 and PHP7.3

--- .webserverfiles/webserverconfig.sh
+++ .webserverfiles/webserverconfig.sh
@@ -16,11 +16,11 @@ debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password vagran
 debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password vagrant"
 
 ###Installing packages 
-sudo apt-get -y -qq install apache2 mysql-server php7.1 php7.1-mysql php7.1-gd php7.1-xml php7.1-mcrypt php7.1-mbstring zip unzip php7.1-zip php7.1-curl php7.1-bcmath openssl phpmyadmin
+sudo apt-get -y -qq install apache2 mysql-server php7.3 php7.3-mysql php7.3-gd php7.3-xml php7.3-mbstring zip unzip php7.3-zip php7.3-curl php7.3-bcmath openssl phpmyadmin
 sudo apt-get -y -qq install php-xdebug
 
 ###Setting PHP cli version to selected version
-sudo update-alternatives --set php /usr/bin/php7.1
+sudo update-alternatives --set php /usr/bin/php7.3
 
 ###generate SSL certificate for server
 openssl req -x509 -out /etc/ssl/certs/localhost.crt -keyout /etc/ssl/private/localhost.key \
@@ -29,11 +29,11 @@ openssl req -x509 -out /etc/ssl/certs/localhost.crt -keyout /etc/ssl/private/loc
    printf "[dn]\nCN=webserver.intranet\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:webserver.intranet\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
 ###Setting up webserver
-sudo apt-get -y install apache2 mysql-server php7.1 php7.1-mysql php7.1-gd php7.1-xml php7.1-mcrypt php7.1-mbstring zip unzip php7.1-zip php7.1-curl
+sudo apt-get -y install apache2 mysql-server php7.3 php7.3-mysql php7.3-gd php7.3-xml php7.3-mcrypt php7.3-mbstring zip unzip php7.3-zip php7.3-curl
 sudo cp /vagrant/.webserverfiles/000-default.conf /etc/apache2/sites-available/000-default.conf
 sudo cp /vagrant/.webserverfiles/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf
-sudo cp /vagrant/.webserverfiles/php.ini /etc/php/7.1/apache2/php.ini
-sudo cp /vagrant/.webserverfiles/xdebug.ini /etc/php/7.1/mods-available/xdebug.ini
+sudo cp /vagrant/.webserverfiles/php.ini /etc/php/7.3/apache2/php.ini
+sudo cp /vagrant/.webserverfiles/xdebug.ini /etc/php/7.3/mods-available/xdebug.ini
 sudo a2enmod rewrite
 sudo a2enmod ssl
 sudo a2ensite default-ssl
@@ -53,8 +53,6 @@ echo "alias drupal='/vagrant/vendor/drupal/console/bin/drupal'" >> /home/vagrant
 
 #Append config directory to default settings
 echo "\$settings['config_sync_directory'] = '../config';" >> /vagrant/web/sites/default/default.settings.php
-#Set *.intranet as trusted host
-echo "\$settings['trusted_host_patterns'] = ['^.+\.intranet$'];" >> /vagrant/web/sites/default/default.settings.php
 
 ###Install drupal and import configuration
 ./vendor/drush/drush/drush -y si minimal --db-url=mysql://root:vagrant@localhost/drupal --config-dir=/vagrant/config --account-pass=admin
